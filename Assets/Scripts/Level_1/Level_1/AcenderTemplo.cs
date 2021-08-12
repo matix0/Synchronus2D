bd55a31d72d31f952d7991ec8aee9446a0cb6981
ThyagoMoura <thyagohenriquemoura@gmail.com>
2021-08-12 14:13:54 -0300

    Estagio 1 cutscenes

--- Assets/Scripts/Level_1/Level_1/AcenderTemplo.cs
+++ Assets/Scripts/Level_1/Level_1/AcenderTemplo.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class AcenderTemplo : MonoBehaviour
 {
+    [HideInInspector]
     public Sprite espadaNoLocal;
+    [HideInInspector]
     public Sprite escudoNoLocal;
+    [HideInInspector]
+    public Sprite espadaForaDoLocal;
+    [HideInInspector]
+    public Sprite escudoForaDoLocal;
+    [HideInInspector]
     public Sprite temploLocal;
+    [HideInInspector]
     public GameObject objectEspada;
+    [HideInInspector]
     public GameObject objectEscudo;
+    [HideInInspector]
     public GameObject acendeTemplo;
+
     private bool espadaTransformada = false;
     private bool escudoTransformado = false;
+
     private Animator animEspada;
     private Animator animEscudo;
+    private Animator animCam;
+
+    [HideInInspector]
+    public Camera currentCamera;
 
     void Start()
     {
         animEspada = objectEspada.GetComponent<Animator>();
         animEscudo = objectEscudo.GetComponent<Animator>();
-        //anim = GetComponent<Animator>();
+        animCam = currentCamera.GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,16 +45,37 @@ public class AcenderTemplo : MonoBehaviour
             objectEspada.GetComponent<SpriteRenderer>().sprite = espadaNoLocal;
             espadaTransformada = true;
         }
+
         if (collision.CompareTag("Escudo"))
         {
             objectEscudo.GetComponent<SpriteRenderer>().sprite = escudoNoLocal;
             escudoTransformado = true;
         }
+
         if (espadaTransformada == true && escudoTransformado == true)
         {
+            //Acender caso espada e escudo no collider
             acendeTemplo.GetComponent<SpriteRenderer>().sprite = temploLocal;
             animEspada.SetTrigger("ImpedirFaderEspada");
             animEscudo.SetTrigger("ImpedirFaderEscudo");
+
+            //Cam animation
+            currentCamera.orthographicSize = 6f;
+            animCam.enabled = true;
+            animCam.SetTrigger("ImpedCam");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision2)
+    {
+        if (collision2.CompareTag("Espada"))
+        {
+            objectEspada.GetComponent<SpriteRenderer>().sprite = espadaForaDoLocal;
+            espadaTransformada = false;
+        }
+        if (collision2.CompareTag("Escudo"))
+        {
+            objectEscudo.GetComponent<SpriteRenderer>().sprite = escudoForaDoLocal;
+            escudoTransformado = false;
         }
     }
 }
