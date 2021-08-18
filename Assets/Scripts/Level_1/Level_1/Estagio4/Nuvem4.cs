36b247ee757e55404db2839abee9c6fbf32db8a4
ThyagoMoura <thyagohenriquemoura@gmail.com>
2021-08-18 15:27:32 -0300

    adicionado rei e lobos alem de barco

--- Assets/Scripts/Level_1/Level_1/Estagio4/Nuvem4.cs
+++ Assets/Scripts/Level_1/Level_1/Estagio4/Nuvem4.cs
@@ -7,15 +7,15 @@ public class Nuvem4 : MonoBehaviour
     private Animator anim;
     public Camera mainCamera;
     public ZoomInZoomOut AcompanhaCamera;
-    private float delay = 2;
-    private float duration = 2;
     // Start is called before the first frame update
     void Start()
-     {
+    {
 
-     }
-     void OnMouseDrag()
-     {
+
+    }
+    void OnMouseDrag()
+    {
+            
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
         Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         transform.position = objPosition;
