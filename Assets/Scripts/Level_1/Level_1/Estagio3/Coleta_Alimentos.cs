62ed4b1db688fb86cdb1abdb75172a93caabb2ff
matix0 <mateusviniciuspw@gmail.com>
2021-08-18 14:43:04 -0300

    cena fornalha no mundo principal

--- Assets/Scripts/Level_1/Level_1/Estagio3/Coleta_Alimentos.cs
+++ Assets/Scripts/Level_1/Level_1/Estagio3/Coleta_Alimentos.cs
@@ -6,18 +6,24 @@ public class Coleta_Alimentos : MonoBehaviour
 {
     private Animator anim;
     public bool Achou_Alimento;
+    private Abre_Tenda Tenda;
+    public BoxCollider2D Colisor;
     // Start is called before the first frame update
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        Colisor.enabled = false;
     }
-
 
     private void OnMouseDown()
     {
+
+
         anim.SetTrigger("Clicou");
         Destroy(gameObject, 1);
         Achou_Alimento = true;
+
+
     }
 }
